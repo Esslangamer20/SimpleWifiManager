cf174107481857f74f2c8511e8fd04f74369a201
Esslangamer20 <esslangamer203@gmail.com>
2026-01-04 16:08:38 -0700

    Actualizar SimpleWifiManager.h

--- SimpleWifiManager.h
+++ SimpleWifiManager.h
@@ -3,10 +3,12 @@
 
 #if defined(ESP32)
   #include <WiFi.h>
-  #include <Preferences.h> // Guardar WiFi en ESP32
+  #include <WebServer.h>
+  #include <Preferences.h>
 #elif defined(ESP8266)
   #include <ESP8266WiFi.h>
-  #include <EEPROM.h>      // Guardar WiFi en ESP8266
+  #include <ESP8266WebServer.h>
+  #include <EEPROM.h>
 #else
   #error "SimpleWiFiManager solo soporta ESP32 y ESP8266"
 #endif
@@ -15,29 +17,29 @@ class SimpleWiFiManager {
 public:
     SimpleWiFiManager();
 
-    // Inicia WiFi (automático si no se pasan parámetros)
-    void begin(const char* ssid = nullptr, const char* password = nullptr);
-
-    // Debe llamarse siempre dentro de loop()
+    void begin();
     void loop();
-
-    // Borra WiFi guardado y reinicia
     void reset();
-
-    // Saber si está conectado a WiFi
     bool isConnected() { return _connected; }
 
 private:
+    const char* _ssid;
+    const char* _password;
+    bool _connected;
+
 #if defined(ESP32)
-    Preferences prefs; // Para guardar SSID/Password
+    Preferences prefs;
+    WebServer server{80};
 #elif defined(ESP8266)
+    WebServer server{80};
     void saveWiFiEEPROM(const char* ssid, const char* password);
     void loadWiFiEEPROM(char* ssid, char* password);
 #endif
 
-    const char* _ssid;
-    const char* _password;
-    bool _connected;
+    void startAP();
+    void startWebServer();
+    void handleRoot();
+    void handleConnect();
 };
 
 #endif
