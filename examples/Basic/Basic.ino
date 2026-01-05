4e56b870bff79a044bfce81be2ea0b4c22498a49
Esslangamer20 <esslangamer203@gmail.com>
2026-01-04 17:21:12 -0700

    Refactor Basic.ino to remove unused serial commands
    Removed serial reset command handling and related messages.

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -6,22 +6,11 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  Serial.println("Iniciando SimpleWiFiManager...");
-
-  // Llamar sin parámetros para WiFi automático / AP
+  // Cambiar modo de salida de datos: Serial o Bluetooth
+  wifi.setDataMode(DATA_SERIAL); // DATA_BLUETOOTH si quieres BT
   wifi.begin();
 }
 
 void loop() {
   wifi.loop();
-
-  // Mensaje por Serial si RESET
-  if (Serial.available()) {
-    String cmd = Serial.readStringUntil('\n');
-    cmd.trim();
-    if (cmd.equalsIgnoreCase("RESET")) {
-      Serial.println("RESET recibido desde Serial...");
-      wifi.reset();
-    }
-  }
 }
